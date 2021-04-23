8d87c9bc7fbafab15ef8bb97e70270ba1d1cb4f3
Francisco Micucci <famicucci@gmail.com>
2021-04-22 21:06:32 -0300

    inicia custon pagination actions

--- components/tablas/TablaPrecios.js
+++ components/tablas/TablaPrecios.js
@@ -1,77 +1,297 @@
 import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
-const StyledTableCell = withStyles((theme) => ({
-	head: {
-		backgroundColor: theme.palette.common.black,
-		color: theme.palette.common.white,
-	},
-	body: {
-		fontSize: 14,
-	},
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
+const useStyles1 = makeStyles((theme) => ({
 	root: {
-		'&:nth-of-type(odd)': {
-			backgroundColor: theme.palette.action.hover,
-		},
+		flexShrink: 0,
+		marginLeft: theme.spacing(2.5),
 	},
-}))(TableRow);
+}));
 
-function createData(name, calories, fat, carbs, protein) {
-	return { name, calories, fat, carbs, protein };
+function TablePaginationActions(props) {
+	const classes = useStyles1();
+	const theme = useTheme();
+	const { count, page, rowsPerPage, onChangePage } = props;
+
+	const handleFirstPageButtonClick = (event) => {
+		onChangePage(event, 0);
+	};
+
+	const handleBackButtonClick = (event) => {
+		onChangePage(event, page - 1);
+	};
+
+	const handleNextButtonClick = (event) => {
+		onChangePage(event, page + 1);
+	};
+
+	const handleLastPageButtonClick = (event) => {
+		onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+	};
+
+	return (
+		<div className={classes.root}>
+			<IconButton
+				onClick={handleFirstPageButtonClick}
+				disabled={page === 0}
+				aria-label="first page"
+			>
+				{theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+			</IconButton>
+			<IconButton
+				onClick={handleBackButtonClick}
+				disabled={page === 0}
+				aria-label="previous page"
+			>
+				{theme.direction === 'rtl' ? (
+					<KeyboardArrowRight />
+				) : (
+					<KeyboardArrowLeft />
+				)}
+			</IconButton>
+			<IconButton
+				onClick={handleNextButtonClick}
+				disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+				aria-label="next page"
+			>
+				{theme.direction === 'rtl' ? (
+					<KeyboardArrowLeft />
+				) : (
+					<KeyboardArrowRight />
+				)}
+			</IconButton>
+			<IconButton
+				onClick={handleLastPageButtonClick}
+				disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+				aria-label="last page"
+			>
+				{theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+			</IconButton>
+		</div>
+	);
+}
+
+TablePaginationActions.propTypes = {
+	count: PropTypes.number.isRequired,
+	onChangePage: PropTypes.func.isRequired,
+	page: PropTypes.number.isRequired,
+	rowsPerPage: PropTypes.number.isRequired,
+};
+
+function createData(codigo, descripcion, precio) {
+	return { codigo, descripcion, precio };
 }
 
 const rows = [
-	createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-	createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-	createData('Eclair', 262, 16.0, 24, 6.0),
-	createData('Cupcake', 305, 3.7, 67, 4.3),
-	createData('Gingerbread', 356, 16.0, 49, 3.9),
+	createData(
+		'AL6V0210UNN',
+		'ALMOHADONES DECO 40X40 CM - CUANDO NECESITABA UNA MANO, ME ENCONTRE CON SU PATA - SIN COLOR - UNICO - VELLON',
+		(2497).toFixed(2)
+	),
+	createData(
+		'CO2G0738EVE',
+		'COLCHONETA LAVABLE PERRO CHICO - PERROS BEIGE - VERDE - S - GUATA',
+		(3245).toFixed(2)
+	),
+	createData(
+		'VV000000059',
+		'COLCHONETA LAVABLE PERRO GRANDE - FRANJA CANICHE/BICHON - ROJO Y NEGRO - L - SIN RELLENO',
+		(4060).toFixed(2)
+	),
+	createData(
+		'CO1G0804DRJ',
+		'COLCHONETA PERRO MEDIANO - PERRITOS MIX - ROJO - M - GUATA',
+		(3124).toFixed(2)
+	),
+	createData(
+		'VV000000054',
+		'CORREA Y COLLAR CHICA - AMARILLA - S',
+		(1300).toFixed(2)
+	),
+	createData(
+		'AL6V0210UNN',
+		'ALMOHADONES DECO 40X40 CM - CUANDO NECESITABA UNA MANO, ME ENCONTRE CON SU PATA - SIN COLOR - UNICO - VELLON',
+		(2497).toFixed(2)
+	),
+	createData(
+		'CO2G0738EVE',
+		'COLCHONETA LAVABLE PERRO CHICO - PERROS BEIGE - VERDE - S - GUATA',
+		(3245).toFixed(2)
+	),
+	createData(
+		'VV000000059',
+		'COLCHONETA LAVABLE PERRO GRANDE - FRANJA CANICHE/BICHON - ROJO Y NEGRO - L - SIN RELLENO',
+		(4060).toFixed(2)
+	),
+	createData(
+		'CO1G0804DRJ',
+		'COLCHONETA PERRO MEDIANO - PERRITOS MIX - ROJO - M - GUATA',
+		(3124).toFixed(2)
+	),
+	createData(
+		'VV000000054',
+		'CORREA Y COLLAR CHICA - AMARILLA - S',
+		(1300).toFixed(2)
+	),
+	createData(
+		'AL6V0210UNN',
+		'ALMOHADONES DECO 40X40 CM - CUANDO NECESITABA UNA MANO, ME ENCONTRE CON SU PATA - SIN COLOR - UNICO - VELLON',
+		(2497).toFixed(2)
+	),
+	createData(
+		'CO2G0738EVE',
+		'COLCHONETA LAVABLE PERRO CHICO - PERROS BEIGE - VERDE - S - GUATA',
+		(3245).toFixed(2)
+	),
+	createData(
+		'VV000000059',
+		'COLCHONETA LAVABLE PERRO GRANDE - FRANJA CANICHE/BICHON - ROJO Y NEGRO - L - SIN RELLENO',
+		(4060).toFixed(2)
+	),
+	createData(
+		'CO1G0804DRJ',
+		'COLCHONETA PERRO MEDIANO - PERRITOS MIX - ROJO - M - GUATA',
+		(3124).toFixed(2)
+	),
+	createData(
+		'VV000000054',
+		'CORREA Y COLLAR CHICA - AMARILLA - S',
+		(1300).toFixed(2)
+	),
+	createData(
+		'AL6V0210UNN',
+		'ALMOHADONES DECO 40X40 CM - CUANDO NECESITABA UNA MANO, ME ENCONTRE CON SU PATA - SIN COLOR - UNICO - VELLON',
+		(2497).toFixed(2)
+	),
+	createData(
+		'CO2G0738EVE',
+		'COLCHONETA LAVABLE PERRO CHICO - PERROS BEIGE - VERDE - S - GUATA',
+		(3245).toFixed(2)
+	),
+	createData(
+		'VV000000059',
+		'COLCHONETA LAVABLE PERRO GRANDE - FRANJA CANICHE/BICHON - ROJO Y NEGRO - L - SIN RELLENO',
+		(4060).toFixed(2)
+	),
+	createData(
+		'CO1G0804DRJ',
+		'COLCHONETA PERRO MEDIANO - PERRITOS MIX - ROJO - M - GUATA',
+		(3124).toFixed(2)
+	),
+	createData(
+		'VV000000054',
+		'CORREA Y COLLAR CHICA - AMARILLA - S',
+		(1300).toFixed(2)
+	),
+	createData(
+		'AL6V0210UNN',
+		'ALMOHADONES DECO 40X40 CM - CUANDO NECESITABA UNA MANO, ME ENCONTRE CON SU PATA - SIN COLOR - UNICO - VELLON',
+		(2497).toFixed(2)
+	),
+	createData(
+		'CO2G0738EVE',
+		'COLCHONETA LAVABLE PERRO CHICO - PERROS BEIGE - VERDE - S - GUATA',
+		(3245).toFixed(2)
+	),
+	createData(
+		'VV000000059',
+		'COLCHONETA LAVABLE PERRO GRANDE - FRANJA CANICHE/BICHON - ROJO Y NEGRO - L - SIN RELLENO',
+		(4060).toFixed(2)
+	),
+	createData(
+		'CO1G0804DRJ',
+		'COLCHONETA PERRO MEDIANO - PERRITOS MIX - ROJO - M - GUATA',
+		(3124).toFixed(2)
+	),
+	createData(
+		'VV000000054',
+		'CORREA Y COLLAR CHICA - AMARILLA - S',
+		(1300).toFixed(2)
+	),
 ];
 
-const useStyles = makeStyles({
+const useStyles2 = makeStyles({
 	table: {
-		minWidth: 700,
+		minWidth: 500,
 	},
 });
 
 const TablaPrecios = () => {
-	const classes = useStyles();
+	const classes = useStyles2();
+	const [page, setPage] = React.useState(0);
+	const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+	const emptyRows =
+		rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage);
+	};
+
+	const handleChangeRowsPerPage = (event) => {
+		setRowsPerPage(parseInt(event.target.value, 10));
+		setPage(0);
+	};
 
 	return (
 		<TableContainer component={Paper}>
-			<Table className={classes.table} aria-label="customized table">
-				<TableHead>
-					<TableRow>
-						<StyledTableCell>Dessert (100g serving)</StyledTableCell>
-						<StyledTableCell align="right">Calories</StyledTableCell>
-						<StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-						<StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-						<StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-					</TableRow>
-				</TableHead>
+			<Table className={classes.table} aria-label="custom pagination table">
 				<TableBody>
-					{rows.map((row) => (
-						<StyledTableRow key={row.name}>
-							<StyledTableCell component="th" scope="row">
-								{row.name}
-							</StyledTableCell>
-							<StyledTableCell align="right">{row.calories}</StyledTableCell>
-							<StyledTableCell align="right">{row.fat}</StyledTableCell>
-							<StyledTableCell align="right">{row.carbs}</StyledTableCell>
-							<StyledTableCell align="right">{row.protein}</StyledTableCell>
-						</StyledTableRow>
+					{(rowsPerPage > 0
+						? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+						: rows
+					).map((row) => (
+						<TableRow key={row.name}>
+							<TableCell component="th" scope="row">
+								{row.codigo}
+							</TableCell>
+							<TableCell style={{ width: 160 }} align="right">
+								{row.descripcion}
+							</TableCell>
+							<TableCell style={{ width: 160 }} align="right">
+								{row.precio}
+							</TableCell>
+						</TableRow>
 					))}
+
+					{emptyRows > 0 && (
+						<TableRow style={{ height: 53 * emptyRows }}>
+							<TableCell colSpan={6} />
+						</TableRow>
+					)}
 				</TableBody>
+				<TableFooter>
+					<TableRow>
+						<TablePagination
+							rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+							colSpan={3}
+							count={rows.length}
+							rowsPerPage={rowsPerPage}
+							page={page}
+							SelectProps={{
+								inputProps: { 'aria-label': 'rows per page' },
+								native: true,
+							}}
+							onChangePage={handleChangePage}
+							onChangeRowsPerPage={handleChangeRowsPerPage}
+							ActionsComponent={TablePaginationActions}
+						/>
+					</TableRow>
+				</TableFooter>
 			</Table>
 		</TableContainer>
 	);
