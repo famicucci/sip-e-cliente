1f4605c42f6da75f05ddbb1da0b522a9834e7ee9
Francisco Micucci <famicucci@gmail.com>
2021-05-18 15:20:52 -0300

    Pagina Home con boton ir a login

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,48 @@
-import Login from '../components/Login';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Link } from '@material-ui/core';
+import AdjustIcon from '@material-ui/icons/Adjust';
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.primary.main,
+	},
+	link: {
+		margin: theme.spacing(3, 0, 2),
+	},
+}));
 
 export default function Home() {
-	return <h1>Desde Home</h1>;
+	const classes = useStyles();
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<AdjustIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sip-e
+				</Typography>
+				<Link href="/login" underline="none">
+					<Button variant="contained" color="primary" className={classes.link}>
+						Ir al login
+					</Button>
+				</Link>
+			</div>
+		</Container>
+	);
 }
