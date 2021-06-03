324d664e0cf30f0f2a9e079fb21235e3772a05ee
Francisco Micucci <famicucci@gmail.com>
2021-06-03 14:57:45 -0300

    Modifica objeto del producto carrito en funcion

--- functions/ventas.js
+++ functions/ventas.js
@@ -1,14 +1,12 @@
 const agregarCarrito = (codigo, ptoStock, lista, filas, carrito) => {
-	const productoCarrito = carrito.find(
-		(fila) => fila.ProductoCodigo === codigo
-	);
+	const productoCarrito = carrito.find((fila) => fila.codigo === codigo);
 
 	// agregar cantidad o agregar al carrito segun corresponda
 	if (productoCarrito) {
 		const nuevaCantidad = productoCarrito.cantidad + 1;
 		const productoModificado = { ...productoCarrito, cantidad: nuevaCantidad };
 
-		const indice = carrito.findIndex((fila) => fila.ProductoCodigo === codigo);
+		const indice = carrito.findIndex((fila) => fila.codigo === codigo);
 
 		carrito.splice(indice, 1, productoModificado);
 	} else {
@@ -19,11 +17,27 @@ const agregarCarrito = (codigo, ptoStock, lista, filas, carrito) => {
 				fila['Producto.Precios.ListaPrecioId'] === lista
 		);
 
-		const productoModificado = { ...producto, cantidad: 1 };
+		// const productoModificado = { ...producto, cantidad: 1 };
+		const productoModificado = {
+			codigo: producto.ProductoCodigo,
+			descripcion: producto['Producto.descripcion'],
+			pu: producto['Producto.Precios.pu'],
+			cantidad: 1,
+			origen: [
+				{
+					alias: 'stock',
+					ptoStockId: producto.PtoStockId,
+					ptoStockDescripcion: producto['PtoStock.descripcion'],
+					cantidad: 1,
+				},
+			],
+		};
+
+		console.log(productoModificado);
 
 		carrito.push(productoModificado);
 	}
-	console.log(carrito);
+
 	return carrito;
 };
 
