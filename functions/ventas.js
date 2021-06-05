1bccd862b603a251de425ace55aac7a95bbede9b
Francisco Micucci <famicucci@gmail.com>
2021-06-05 15:26:50 -0300

    Refactoriza agregarCarrito

--- functions/ventas.js
+++ functions/ventas.js
@@ -1,133 +1,158 @@
 const agregarCarrito = (codigo, ptoStock, lista, filas, carrito) => {
-	const producto = filas.find(
+	let producto;
+	let productoCarrito;
+	let filaOrigen;
+
+	// busca el codigo en las filas
+	producto = filas.find(
 		(fila) =>
 			fila.ProductoCodigo === codigo &&
 			fila.PtoStockId === ptoStock &&
 			fila['Producto.Precios.ListaPrecioId'] === lista
 	);
+	// busca el codigo en el carrito
+	productoCarrito = carrito.find((fila) => fila.codigo === codigo);
 
-	const productoCarrito = carrito.find((fila) => fila.codigo === codigo);
+	if (productoCarrito && producto.cantidad > 0) {
+		// revisa si existe el pto stock
+		filaOrigen = productoCarrito.origen.find(
+			(fila) => fila.ptoStockId === ptoStock
+		);
+	} else if (productoCarrito && producto.cantidad <= 0) {
+		// revisa si existe la fila origen producción
+		filaOrigen = productoCarrito.origen.find((fila) => fila.ptoStockId === 0);
+	}
 
-	let productoModificado = {};
-	// agregar cantidad o agregar al carrito segun corresponda
-	if (productoCarrito) {
-		if (producto.cantidad > 0) {
-			const nuevaCantidad = productoCarrito.cantidad + 1;
-			productoModificado = { ...productoCarrito, cantidad: nuevaCantidad };
-
-			// revisa si el pto stock ya existe
-			const filaOrigen = productoModificado.origen.find(
-				(fila) => fila.ptoStockId === ptoStock
-			);
-
-			// trae el origen actual del producto
-			const origenProductoModificado = productoModificado.origen;
-
-			if (filaOrigen) {
-				// si existe calcula nueva cantidad
-				const nuevaCantidad = filaOrigen.cantidad + 1;
-				const filaOrigenModificada = {
-					...filaOrigen,
-					cantidad: nuevaCantidad,
-				};
-
-				const origenModificado = origenProductoModificado.map((fila) =>
-					fila.ptoStockId === ptoStock ? filaOrigenModificada : fila
-				);
-
-				productoModificado = {
-					...productoModificado,
-					origen: origenModificado,
-				};
-			} else {
-				const nuevoOrigen = {
+	if (!productoCarrito && producto.cantidad > 0) {
+		productoCarrito = {
+			codigo: producto.ProductoCodigo,
+			descripcion: producto['Producto.descripcion'],
+			pu: producto['Producto.Precios.pu'],
+			cantidad: 1,
+			origen: [
+				{
 					alias: 'stock',
 					ptoStockId: producto.PtoStockId,
 					ptoStockDescripcion: producto['PtoStock.descripcion'],
 					cantidad: 1,
-				};
+				},
+			],
+		};
+		carrito.push(productoCarrito);
+	} else if (!productoCarrito && producto.cantidad <= 0) {
+		productoCarrito = {
+			codigo: producto.ProductoCodigo,
+			descripcion: producto['Producto.descripcion'],
+			pu: producto['Producto.Precios.pu'],
+			cantidad: 1,
+			origen: [
+				{
+					alias: 'produccion',
+					ptoStockId: 0,
+					ptoStockDescripcion: 'produccion',
+					cantidad: 1,
+				},
+			],
+		};
+		carrito.push(productoCarrito);
+	} else if (productoCarrito && producto.cantidad > 0 && !filaOrigen) {
+		const nuevoOrigen = {
+			alias: 'stock',
+			ptoStockId: producto.PtoStockId,
+			ptoStockDescripcion: producto['PtoStock.descripcion'],
+			cantidad: 1,
+		};
+		productoCarrito.origen.push(nuevoOrigen);
 
-				const origenProductoModificado = productoModificado['origen'];
-				origenProductoModificado.push(nuevoOrigen);
-			}
+		const nuevaCantidadProducto = productoCarrito.cantidad + 1;
+		productoCarrito = { ...productoCarrito, cantidad: nuevaCantidadProducto };
 
-			// sobreescribe carrito
-			const carritoModificado = carrito.map((fila) =>
-				fila.codigo === productoModificado.codigo ? productoModificado : fila
-			);
+		// sobreescribe carrito
+		const carritoModificado = carrito.map((fila) =>
+			fila.codigo === productoCarrito.codigo ? productoCarrito : fila
+		);
 
-			carrito = [...carritoModificado];
-		} else {
-			const nuevaCantidad = productoCarrito.cantidad + 1;
-			productoModificado = { ...productoCarrito, cantidad: nuevaCantidad };
+		carrito = [...carritoModificado];
+	} else if (productoCarrito && producto.cantidad > 0 && filaOrigen) {
+		const cantidad = filaOrigen.cantidad + 1;
+		filaOrigen = {
+			...filaOrigen,
+			cantidad: cantidad,
+		};
 
-			const filaOrigenProduccion = productoModificado.origen.find(
-				(fila) => fila.alias === 'produccion'
-			);
+		const origenModificado = productoCarrito.origen.map((fila) =>
+			fila.ptoStockId === ptoStock ? filaOrigen : fila
+		);
 
-			const nuevaCantidadOrigen = filaOrigenProduccion.cantidad + 1;
-			const filaOrigenProduccionModificada = {
-				...filaOrigenProduccion,
-				cantidad: nuevaCantidadOrigen,
-			};
+		productoCarrito = {
+			...productoCarrito,
+			origen: origenModificado,
+		};
 
-			const origenModificado = productoModificado.origen.map((fila) =>
-				fila.alias === 'produccion' ? filaOrigenProduccionModificada : fila
-			);
+		const nuevaCantidadProducto = productoCarrito.cantidad + 1;
+		productoCarrito = { ...productoCarrito, cantidad: nuevaCantidadProducto };
 
-			productoModificado = { ...productoModificado, origen: origenModificado };
+		// sobreescribe carrito
+		const carritoModificado = carrito.map((fila) =>
+			fila.codigo === productoCarrito.codigo ? productoCarrito : fila
+		);
 
-			// sobreescribe carrito
-			const carritoModificado = carrito.map((fila) =>
-				fila.codigo === productoModificado.codigo ? productoModificado : fila
-			);
+		carrito = [...carritoModificado];
+	} else if (productoCarrito && producto.cantidad <= 0 && !filaOrigen) {
+		const nuevoOrigen = {
+			alias: 'produccion',
+			ptoStockId: 0,
+			ptoStockDescripcion: 'produccion',
+			cantidad: 1,
+		};
+		productoCarrito.origen.push(nuevoOrigen);
 
-			carrito = [...carritoModificado];
-		}
-	} else {
-		if (producto.cantidad > 0) {
-			productoModificado = {
-				codigo: producto.ProductoCodigo,
-				descripcion: producto['Producto.descripcion'],
-				pu: producto['Producto.Precios.pu'],
-				cantidad: 1,
-				origen: [
-					{
-						alias: 'stock',
-						ptoStockId: producto.PtoStockId,
-						ptoStockDescripcion: producto['PtoStock.descripcion'],
-						cantidad: 1,
-					},
-				],
-			};
-			carrito.push(productoModificado);
-		} else {
-			productoModificado = {
-				codigo: producto.ProductoCodigo,
-				descripcion: producto['Producto.descripcion'],
-				pu: producto['Producto.Precios.pu'],
-				cantidad: 1,
-				origen: [
-					{
-						alias: 'produccion',
-						ptoStockId: 0,
-						ptoStockDescripcion: 'produccion',
-						cantidad: 1,
-					},
-				],
-			};
-			carrito.push(productoModificado);
-		}
+		const nuevaCantidadProducto = productoCarrito.cantidad + 1;
+		productoCarrito = { ...productoCarrito, cantidad: nuevaCantidadProducto };
+
+		// sobreescribe carrito
+		const carritoModificado = carrito.map((fila) =>
+			fila.codigo === productoCarrito.codigo ? productoCarrito : fila
+		);
+
+		carrito = [...carritoModificado];
+	} else if (productoCarrito && producto.cantidad <= 0 && filaOrigen) {
+		const cantidad = filaOrigen.cantidad + 1;
+		filaOrigen = {
+			...filaOrigen,
+			cantidad: cantidad,
+		};
+
+		const origenModificado = productoCarrito.origen.map((fila) =>
+			fila.ptoStockId === 0 ? filaOrigen : fila
+		);
+
+		productoCarrito = {
+			...productoCarrito,
+			origen: origenModificado,
+		};
+
+		const nuevaCantidadProducto = productoCarrito.cantidad + 1;
+		productoCarrito = { ...productoCarrito, cantidad: nuevaCantidadProducto };
+
+		// sobreescribe carrito
+		const carritoModificado = carrito.map((fila) =>
+			fila.codigo === productoCarrito.codigo ? productoCarrito : fila
+		);
+
+		carrito = [...carritoModificado];
 	}
 
 	return carrito;
 };
 
+// const restaCantidadEnStock = (codigo, ptoStock, filas, cantidad) => {
+// 	const producto = filas.find(
+// 		(fila) => fila.ProductoCodigo === codigo && fila.PtoStockId === ptoStock
+// 	);
+
+// 	const nuevaCantidad = producto.cantidad - cantidad;
+// 	producto = { ...producto, cantidad: nuevaCantidad };
+// };
+
 export { agregarCarrito };
-
-// productoCarrito === 'true' && producto.cantidad > 0 && filaOrigen === 'true' ---> cambio la cantidad
-// productoCarrito === 'true' && producto.cantidad > 0 && filaOrigen === 'false' ---> hago un push
-// productoCarrito === 'true' && producto.cantidad <= 0 ---> modifica directo la cantidad en produccion
-
-// productoCarrito === 'false' && producto.cantidad > 0
-// productoCarrito === 'false' && producto.cantidad <= 0
