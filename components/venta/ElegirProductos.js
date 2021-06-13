dc533abc649e502dcfe60495b13bcd0595be623c
Francisco Micucci <famicucci@gmail.com>
2021-06-12 22:59:16 -0300

    Componente Lector elegir producto, lineas de prog

--- components/venta/ElegirProductos.js
+++ components/venta/ElegirProductos.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BuscarProducto from './BuscarProducto';
 import RadioElegirProductos from './RadioElegirProductos';
 import TablaElegirProducto from '../tablas/TablaElegirProducto';
 import SelectListaPrecio from './SelectListaPrecioVenta';
 import Paper from '@material-ui/core/Paper';
+import VentasContext from '../../context/ventas/ventasContext';
+import LectorElegirProducto from './LectorElegirProducto';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
 	paper: {
@@ -17,13 +20,32 @@ const useStyles = makeStyles((theme) => ({
 const ElegirProductos = () => {
 	const classes = useStyles();
 
+	const { modo } = useContext(VentasContext);
+
 	return (
-		<Paper className={classes.paper} variant="outlined">
-			<BuscarProducto />
-			<RadioElegirProductos />
-			<SelectListaPrecio />
-			<TablaElegirProducto />
-		</Paper>
+		<>
+			{modo === 'manual' ? (
+				<Paper className={classes.paper} variant="outlined">
+					<BuscarProducto />
+					<RadioElegirProductos />
+					<SelectListaPrecio />
+					<TablaElegirProducto />
+				</Paper>
+			) : (
+				<Grid
+					container
+					spacing={0}
+					direction="column"
+					alignItems="center"
+					justify="center"
+					style={{ minHeight: '86vh' }}
+				>
+					<Grid item xs={3}>
+						<LectorElegirProducto />
+					</Grid>
+				</Grid>
+			)}
+		</>
 	);
 };
 
---
+++ components/venta/LectorElegirProducto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LineaEsperandoCodigo from '../../components/venta/LineaEsperandoCodigo';
+
+const LectorElegirProducto = () => {
+	return (
+		<div>
+			<LineaEsperandoCodigo />
+		</div>
+	);
+};
+
+export default LectorElegirProducto;
