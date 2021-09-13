2eb928d503f8986fbcae8c4c6fc71ac8f5b05903
Francisco Micucci <famicucci@gmail.com>
2021-09-13 12:46:03 -0300

    Show bar chart

--- components/reporte/ChartGrossRevenue.js
+++ components/reporte/ChartGrossRevenue.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import GlobalDataContext from '../../context/globalData/GlobalDataContext';
 import moment from 'moment';
 
 const ChartGrossRevenue = () => {
-	const { invoices, getInvoicing } = useContext(GlobalDataContext);
+	const { startDate, endDate, invoices, getInvoicing } =
+		useContext(GlobalDataContext);
 
 	const [chartData, setChartData] = useState({});
 
-	const startDate = '2021-07-02';
-	const endDate = '2021-10-04';
+	useEffect(() => {
+		if (invoices.length === 0) {
+			getInvoicing(startDate, endDate);
+		}
+
+		chart(startDate, endDate);
+	}, [invoices, startDate, endDate]);
 
 	const chart = (startDate, endDate) => {
 		const startMonth = moment(startDate, 'YYYY/MM/DD').format('M');
@@ -40,8 +46,9 @@ const ChartGrossRevenue = () => {
 				{
 					label: 'Ingresos totales',
 					data: Object.values(amountPerMonth),
-					borderColor: 'rgb(75, 192, 192)',
-					borderWidth: 3,
+					borderColor: 'rgba(75, 192, 192)',
+					backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+					borderWidth: 2,
 					tension: 0.1,
 				},
 				// {
@@ -55,16 +62,8 @@ const ChartGrossRevenue = () => {
 		});
 	};
 
-	useEffect(() => {
-		getInvoicing(startDate, endDate);
-	}, []);
-
-	useEffect(() => {
-		chart(startDate, endDate);
-	}, [invoices]);
-
 	return (
-		<Line
+		<Bar
 			data={chartData}
 			options={{
 				responsive: true,
