2352e87a83a4d5220fc7c7d8a87943a728107d38
Francisco Micucci <famicucci@gmail.com>
2021-10-05 22:39:22 -0300

    Hide Reportes to not admin users

--- components/layouts/Menu/Lista.js
+++ components/layouts/Menu/Lista.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { List, Divider } from '@material-ui/core';
-
 import Precios from './Precios';
 import Ventas from './Ventas';
 import Stock from './Stock';
 import Clientes from './Clientes';
 import Gastos from './Gastos';
 import Reportes from './Reportes';
+import authContext from '../../../context/autenticacion/authContext';
 
 const Lista = () => {
+	const { usuario } = useContext(authContext);
+
 	return (
 		<List component="nav">
 			<Precios />
@@ -21,7 +23,7 @@ const Lista = () => {
 
 			<Gastos />
 
-			<Reportes />
+			{usuario ? <>{usuario.rol ? <Reportes /> : null}</> : null}
 
 			<Divider />
 		</List>
